ce42f65e18867724ec7174ecfb3a9c8dfa1ac123
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-05-07 15:49:43 -0300

    fix(FakeSGPLI): paper

--- src/main/java/code/blurone/cowardless/FakeSGPLI.kt
+++ src/main/java/code/blurone/cowardless/FakeSGPLI.kt
@@ -11,8 +11,8 @@ import net.minecraft.util.FutureChain
 import org.slf4j.Logger
 import java.lang.reflect.Field
 
-class FakeSGPLI(private val fakePlayerListUtil: FakePlayerListUtil, minecraftserver: MinecraftServer?,
-                networkmanager: Connection?, entityplayer: ServerPlayer?, commonlistenercookie: CommonListenerCookie?
+class FakeSGPLI(private val fakePlayerListUtil: FakePlayerListUtil, minecraftserver: MinecraftServer,
+                networkmanager: Connection, entityplayer: ServerPlayer, commonlistenercookie: CommonListenerCookie
 ) : ServerGamePacketListenerImpl(minecraftserver, networkmanager, entityplayer, commonlistenercookie) {
 
     companion object {
@@ -23,23 +23,34 @@ class FakeSGPLI(private val fakePlayerListUtil: FakePlayerListUtil, minecraftser
 
     private val chatMessageChain = CHAT_MESSAGE_CHAIN_FIELD.get(this) as FutureChain
 
-    override fun onDisconnect(ichatbasecomponent: Component?) {
-        if (!processedDisconnect) {
-            processedDisconnect = true
-            LOGGER.info(
-                "{} lost connection: {}",
-                player.name.string, ichatbasecomponent!!.string
-            )
-            removeFakePlayerFromWorld()
-        }
+    override fun onDisconnect(reason: Component, quitMessage: net.kyori.adventure.text.Component?) {
+        // Paper end - Fix kick event leave message not being sent
+        // CraftBukkit start - Rarely it would send a disconnect line twice
+        if (this.processedDisconnect)
+            return
+        else
+            this.processedDisconnect = true
+
+        // CraftBukkit end
+        LOGGER.info("{} lost connection: {}", player.name.string, reason.string)
+        removeFakePlayerFromWorld() // Paper - Fix kick event leave message not being sent
     }
 
-    private fun removeFakePlayerFromWorld()
-    {
-        chatMessageChain.close()
-        player.disconnect()
-        fakePlayerListUtil.removeFake(this.player)
+    private fun removeFakePlayerFromWorld() {
+        // Paper end - Fix kick event leave message not being sent
+        this.chatMessageChain.close()
 
+        // CraftBukkit start - Replace vanilla quit message handling with our own.
+        /*
+        this.server.invalidateStatus();
+        this.server.getPlayerList().broadcastSystemMessage(IChatBaseComponent.translatable("multiplayer.player.left", this.player.getDisplayName()).withStyle(EnumChatFormat.YELLOW), false);
+        */
+        player.disconnect()
+
+        // Paper start - Adventure
+        fakePlayerListUtil.removeFake(player)// Paper - pass in quitMessage to fix kick message not being used
+
+        // CraftBukkit end
         player.textFilter.leave()
     }
 }
