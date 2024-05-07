64fd2bd5258038f5bab6baff353212f738920ed7
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-05-07 15:49:59 -0300

    fix(FakeConnection): paper

--- src/main/java/code/blurone/cowardless/FakeConnection.kt
+++ src/main/java/code/blurone/cowardless/FakeConnection.kt
@@ -7,16 +7,8 @@ import net.minecraft.network.PacketListener
 import net.minecraft.network.protocol.PacketFlow
 
 class FakeConnection(enumprotocoldirection: PacketFlow) : Connection(enumprotocoldirection) {
-    override fun setListener(packetlistener: PacketListener?) {
+    override fun setListener(packetlistener: PacketListener) {
         val embeddedChannel = EmbeddedChannel(this)
         embeddedChannel.attr(ATTRIBUTE_SERVERBOUND_PROTOCOL).set(ConnectionProtocol.PLAY.codec(PacketFlow.SERVERBOUND))
-        /*
-        Validate.notNull(packetlistener, "packetListener", *arrayOfNulls(0))
-        val enumprotocoldirection = packetlistener!!.flow()
-        check(enumprotocoldirection == this.receiving) { "Trying to set listener for wrong side: connection is " + this.receiving + ", but listener is " + enumprotocoldirection }
-        val enumprotocol = packetlistener!!.protocol()
-        check(this.packetListener!!.protocol() == enumprotocol) { "Trying to set listener for protocol " + enumprotocol.id() + ", but current " + enumprotocoldirection + " protocol is " + enumprotocol1.id() }
-        this.packetListener = packetlistener
-        this.disconnectListener = null*/
     }
 }
