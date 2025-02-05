e6a1fdc6d3afcbf4713a931b91212505a378067e
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2025-02-05 17:23:06 -0300

    chore: added paper-plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ tasks.processResources {
     val props = mapOf("version" to version)
     inputs.properties(props)
     filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
+    filesMatching("*plugin.yml") {
         expand(props)
     }
 }
