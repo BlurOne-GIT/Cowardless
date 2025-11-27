05003bbf4a10ad8124b8eda896663fc83b20d70e
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2025-11-27 13:09:04 -0300

    fix: wrong property name

--- plugin/src/main/java/code/blurone/cowardless/CowardlessPaper.kt
+++ plugin/src/main/java/code/blurone/cowardless/CowardlessPaper.kt
@@ -31,7 +31,7 @@ class CowardlessPaper : JavaPlugin(), Listener {
     private val redWarning = config.getBoolean("red_warning", false)
     private val pvpOnly = config.getBoolean("pvp_only", false)
     private val twoSided = config.getBoolean("two_sided_pvp", true)
-    private val actionBar = config.getBoolean("action_bar", true)
+    private val actionBar = config.getBoolean("action_bar_message", true)
     private val chatMessages = config.getBoolean("chat_message", true)
     private val actionBarRunnables: MutableMap<String, BukkitRunnable> = mutableMapOf()
     private val redUnwarnScheduledTasks: MutableMap<String, ScheduledTask> = mutableMapOf()
